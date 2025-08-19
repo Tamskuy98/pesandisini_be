ecd2a1ba75cf890957bb393e72f31a40eedeffe1
tama <aldpratamaa@gmail.com>
2025-08-19 23:33:07 +0700

    repair function userorder

--- app/controller/sendtowatshapp.js
+++ app/controller/sendtowatshapp.js
@@ -1,39 +1,28 @@
 const { getOrder } = require('../helper/orderservice.js');
-const { getClient } = require('../helper/auth.js');
+const { error } = require('qrcode-terminal');
 
 
 exports.userorder = async (req, res)=>{
-let _err = [];
+const _err = [];
+const data = req.body.data;
 
-const client = getClient();
-
-const data = req.body?.data;
 if(!data || data == null){
-  return res.status(400).json({succes: true, message: "Bad Request", data: _err});
-}
-if(typeof data !== 'object'){
-  return res.status(400).json({success: false, message: "Bad Request", data: _err});
+  if (typeof data !== 'object'){
+    if(!Array.isArray(data)){
+      return res.status(400).json({succes: true, message: "Bad Request", data: _err});
+    }
+      return res.status(400).json({succes: true, message: "Bad Request", data: _err});
   }
-if(!Array.isArray(data)){
-  return res.status(400).json({succes: true, message: "Bad Request", data: _err});
+  return res.status(400).json({succes: true, message: "Bad Request", data: _err}); 
 }
 
 const result = await getOrder(data);
-const {number, message} = result[0];
-const isRegistered = await client.isRegisteredUser(number);
-if (!isRegistered) {
-  return res.status(400).json({succes: true, message: "Number not registered", data: _err});
+console.log(` ini nmr setelah getOrder ${result.number}`);
+if(result.error) {
+   return res.status(400).json({succes: true, message: result.error, data:null});
 }
-if (!message || message === '') {
-  return res.status(400).json({succes: true, message: "Message undefined", data: _err});
-}
-try {
-  // const messagex = "ini pesanan nya";
-  await client.sendMessage(number, message);
-  return res.status(200).json({ success: true, message: 'Succes sent to Whatsapp' });
-} catch (error) {
-  console.error(_err , error);
-  return res.status(500).json({ success: false, message: 'Error sent to Whatsapp', error: error.message });
+if(result.success) {
+   return res.status(400).json({succes: true, message: result.success, data:result});
 }
 }
 
