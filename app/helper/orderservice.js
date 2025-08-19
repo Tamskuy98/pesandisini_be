a7f74e8dec8024b931e8a90c686248dccbfe1fce
tama <aldpratamaa@gmail.com>
2025-08-19 23:33:33 +0700

    repair orderservice

--- app/helper/orderservice.js
+++ app/helper/orderservice.js
@@ -1,31 +1,42 @@
+const { getClient } = require('../helper/auth.js');
 
 
-
-exports.getOrder = async (order, res) =>{
+exports.getOrder = async (data, res) =>{
   
-  const data = order;  
-  const returndata = [];
-  if(!data){
-    return {error: "data tidak ditemukan"};
+  const client = getClient();
+  
+  // validasi number
+  const unnumberd = data[0].numberPhone.replace(/\D/g, '');
+  if(!unnumberd || unnumberd == ""){
+    return {error: "Nomor telepon wajib di isi"};
   }
-   
-  //  numberPhone = nomor + "  "
-  data.forEach(item => {
-  const number = item.numberPhone +  "@c.us";
-  const orders = item.order;
-
-    if (!number || !Array.isArray(orders)) {
-      return {error: "data undefined"};
-    }
-    if (!number || number == '' || number == null) {
-      return {error: "Mohon mengisi nomor telepon"};
-    }
-    //declare var
-  const itemList = orders.map((order, index) => {
+  if (!/^62\d{8,13}$/.test(unnumberd)){
+    return {error: "Nomor telepon tidak valid"};
+  }
+  
+  const number = unnumberd +  "@c.us";
+  
+  const isRegistered = await client.isRegisteredUser(number);
+  if (!isRegistered) {
+  return {error: "Nomor Whatsapp tidak terdaftar"};
+ }
+  
+  //validasi massage
+  const orderlist = data[1].order;
+  if(!orderlist || orderlist == null){
+    return {error: "Data order tidak ditemukan"};
+  }
+  if(!orderlist || orderlist == null){
+    return {error: "Data order tidak ditemukan"};
+  }
+  if(!Array.isArray(orderlist)){
+    return {error: "Data order tidak valid"};
+  }
+  const itemList = orderlist.map((order, index) => {
   return `${index + 1}. ${order.menu} = ${order.quantity}`;
-  }).join('\n'); // gabungkan jadi satu string dengan newline
+  }).join('\n'); 
     
-const message = `
+  const message = `
 📦 *Konfirmasi Pesanan*
 Hallo kak, terima kasih telah melakukan pemesanan. Berikut detail pesananmu:
 
@@ -33,10 +44,15 @@ ${itemList}
 
 ✅ Mohon ditunggu, kami akan mengirimkan total tagihannya. Untuk bukti pembayaran bisa dilampirkan pada chat ini agar pesanan bisa diproses dan akan segera dikirim.
 Terima kasih sudah berbelanja di toko kami 🙏
-`; 
+`;
 
-returndata.push({number, message});
-    });
-    return returndata;
+//send to whatsapp
+try {
+  await client.sendMessage(number, message);
+  return  {success: "Data Pesanan telah dikirim ke whatsapp"};
+} catch (error) {
+  console.error(_err , error);
+  return  {errpr: "Data Pesanan gagal terkirim ke whatsapp"};
+}
 }
 
